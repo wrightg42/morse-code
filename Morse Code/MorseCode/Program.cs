49e1404f920566b365dff10da044361a5c901102
It423 <it42@hotmail.co.uk>
2014-11-03 19:18:46 +0000

    Create project

---
+++ Morse Code/MorseCode/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program.cs
+// <copyright file="Program.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
