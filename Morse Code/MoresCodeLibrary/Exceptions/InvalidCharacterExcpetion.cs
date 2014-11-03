85aa26076f5e8e82b5f0cf017439c4b6b2cc2fcf
It423 <it42@hotmail.co.uk>
2014-11-03 22:09:42 +0000

    Create an invalid character exception

---
+++ Morse Code/MoresCodeLibrary/Exceptions/InvalidCharacterExcpetion.cs
@@ -0,0 +1,32 @@
+﻿// InvalidCharacterExcpetion.cs
+// <copyright file="InvalidCharacterExcpetion.cs"> This code is protected under the MIT License. </copyright>
+using System;
+
+namespace MoresCodeLibrary.Exceptions
+{
+    /// <summary>
+    /// An exception for invalid/unrecognised characters being used.
+    /// </summary>
+    public class InvalidCharacterExcpetion : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCharacterExcpetion" /> class.
+        /// </summary>
+        /// <param name="character"> The character (in morse or english form). </param>
+        /// <param name="isInMorse"> Whether the character is in morse form. </param>
+        public InvalidCharacterExcpetion(string character, bool isInMorse)
+        {
+            this.Character = character;
+        }
+
+        /// <summary>
+        /// Gets or sets the character that was not invalid/recognised.
+        /// </summary>
+        public string Character { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the character is in morse form.
+        /// </summary>
+        public bool IsInMorse { get; set; }
+    }
+}
