987a658b55abe2886196c80625ad159345fcb77c
It423 <it42@hotmail.co.uk>
2014-11-05 19:53:46 +0000

    Add a conversion to english from morse code

---
+++ Morse Code/MoresCodeLibrary/Conversions/ConvertFromMorse.cs
@@ -0,0 +1,62 @@
+﻿// ConvertFromMorse.cs
+// <copyright file="ConvertFromMorse.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeLibrary.Conversions
+{
+    /// <summary>
+    /// A static class to convert from morse code to english.
+    /// </summary>
+    public static class ConvertFromMorse
+    {
+        /// <summary>
+        /// Converts a message from morse to english.
+        /// </summary>
+        /// <param name="message"> The message. </param>
+        /// <returns> The english of the message. </returns>
+        public static string Convert(string message)
+        {
+            // Only convert if it is valid morse
+            if (!Validation.ValidMorse(message))
+            {
+                return null;
+            }
+
+            // Copy each character into the result string
+            string res = string.Empty;
+            foreach (string word in message.Split(new char[] { '\\', '|', '/' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                foreach (string letter in word.Split(' '))
+                {
+                    res += FindLetter(letter);
+                }
+
+                res += ' ';
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Finds the character value of a morse code character.
+        /// </summary>
+        /// <param name="morseVal"> The morse code of the character. </param>
+        /// <returns> The english character. </returns>
+        public static char FindLetter(string morseVal)
+        {
+            foreach (char c in MorseCharacters.MorseCharacterValues.Keys)
+            {
+                if (MorseCharacters.MorseCharacterValues[c] == morseVal)
+                {
+                    return c;
+                }
+            }
+
+            return new char();
+        }
+    }
+}
