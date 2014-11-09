0eb8218f8b40b5c526043f246223445380cebafa
It423 <it42@hotmail.co.uk>
2014-11-09 20:31:58 +0000

    Add regex for text boxes

--- Morse Code/MorseCode/MainWindow.xaml.cs
+++ Morse Code/MorseCode/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿// MainWindow.xaml.cs
+// <copyright file="MainWindow.xaml.cs"> This code is protected under the MIT License. </copyright>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,40 @@ namespace MorseCode
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Makes sure that the english text box contains only recognized characters.
+        /// </summary>
+        /// <param name="sender"> What raised the event. </param>
+        /// <param name="e"> The event arguments. </param>
+        private void English_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // The regex pattern
+            Regex rgx = new Regex("[^a-zA-Z0-9 .,?!'\"()&:;=+-/_$@%]");
+
+            // Remove all character from the textbox that arn't in the regex
+            this.English.Text = rgx.Replace(this.English.Text, string.Empty);
+        }
+
+        /// <summary>
+        /// Makes sure that the morse code text box contains only characters for morse code strings.
+        /// </summary>
+        /// <param name="sender"> What raised the event. </param>
+        /// <param name="e"> The event arguments. </param>
+        private void Morse_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // The regex pattern
+            Regex rgx = new Regex("[^-. |]");
+            
+            // Remove all character from the textbox that arn't in the regex
+            this.Morse.Text = rgx.Replace(this.Morse.Text, string.Empty);
         }
     }
 }
