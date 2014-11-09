b85d0c8bfd8384c2d716c30683db19ae6b45523c
It423 <it42@hotmail.co.uk>
2014-11-09 21:34:21 +0000

    Remove unused usings

--- Morse Code/MorseCode/MainWindow.xaml.cs
+++ Morse Code/MorseCode/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿// MainWindow.xaml.cs
 // <copyright file="MainWindow.xaml.cs"> This code is protected under the MIT License. </copyright>
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Media;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MorseCodeLibrary.Conversions;
 
 namespace MorseCode
@@ -114,7 +102,7 @@ namespace MorseCode
         /// <param name="e"> The event arguments. </param>
         private void Play_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
     }
 }
